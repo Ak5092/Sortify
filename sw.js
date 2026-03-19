2a221adc7b74dd6e662e2aee11be78738055f3e4
Ak5092 <akshatsaraswat00@gmail.com>
2026-03-19 13:26:49 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-    // Standard passthrough for Spotify API calls
-    event.respondWith(fetch(event.request));
-});
